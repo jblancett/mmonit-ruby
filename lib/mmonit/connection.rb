595a6672f24ff22b48110e506c51861968277e7b
Josh Blancett <josh@firma8.com>
2013-05-15 15:29:04 -0400

    get users list

--- lib/mmonit/connection.rb
+++ lib/mmonit/connection.rb
@@ -49,6 +49,10 @@ module MMonit
 			JSON.parse(self.request('/json/admin/hosts/list').body)['records']
 		end
 
+		def users
+			JSON.parse(self.request('/json/admin/users/list').body)['records']
+		end
+
 		def request(path, body="", headers = {})
 			self.connect unless @http.is_a?(Net::HTTP)
 			@http.post(path, body, @headers.merge(headers))
